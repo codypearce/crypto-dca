3af7473851e30e2f8f2f9ccdca0c01aeef9fdeff
codypearce <codyapearce@gmail.com>
2018-08-26 20:47:47 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./pages/Home/index";
+
+class App extends Component {
+  render() {
+    return <Home />;
+  }
+}
+
+export default App;
