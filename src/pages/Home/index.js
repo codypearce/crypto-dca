b9a7181717b70df5facfbad53a897852f44fb1b3
codypearce <codyapearce@gmail.com>
2018-08-27 07:38:32 -0400

    set up layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import "./index.css";
 
 import Header from "./Components/Header";
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="home">
         <Header />
+        <div className="home_body row center-xs middle-xs">Test</div>
       </div>
     );
   }
