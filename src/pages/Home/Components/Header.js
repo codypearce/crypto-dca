1ee7dde9c44d30d548d6faf72dccefe25659b430
codypearce <codyapearce@gmail.com>
2018-08-27 07:31:04 -0400

    style header

--- src/pages/Home/Components/Header.js
+++ src/pages/Home/Components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="row between-xs middle-xs header">
+      <h3>Crypto DCA</h3>
+      <div className="row">
+        <a href="/" className="link header_link">
+          Home
+        </a>
+        <a href="/about" className="link header_link">
+          About
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
