169cd18dce83f84d6cea26fe446befea8fdfe8aa
codypearce <codyapearce@gmail.com>
2018-08-28 07:22:59 -0400

    add frequency and amount, method to update state

--- src/pages/Home/DatesForm/index.js
+++ src/pages/Home/DatesForm/index.js
@@ -2,13 +2,36 @@ import React, { Component } from "react";
 import "./index.css";
 
 class DatesForm extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Bitcoin DCA</h1>
-      </div>
-    );
-  }
+	state = {
+		frequency: null,
+		amount: null,
+		startDate: null,
+		endDate: null
+	};
+
+	_updateValue(type, value) {
+		this.setState({
+			[type]: value.target.value
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Bitcoin DCA</h1>
+				<input
+					placeholder="How frequent?"
+					type="number"
+					onChange={e => this._updateValue("frequency", e)}
+				/>
+				<input
+					placeholder="How much?"
+					type="number"
+					onChange={e => this._updateValue("amount", e)}
+				/>
+			</div>
+		);
+	}
 }
 
 export default DatesForm;
