25d48c78f66272b78b15e134f24184c6d44d62a1
codypearce <outis161@gmail.com>
2018-09-07 05:09:51 -0400

    header redesign, add image background

--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -3,13 +3,12 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="row between-xs middle-xs header">
-      <h3>Crypto DCA</h3>
+    <header className="row start-xs middle-xs header">
       <div className="row">
         <a href="/" className="link header_link">
           Home
         </a>
-        <a href="/about" className="link header_link">
+        <a href="/" className="link header_link">
           About
         </a>
       </div>
