c16d85b7057d9846dec59e69f599e76e6ff3bef1
codypearce <outis161@gmail.com>
2018-10-23 09:01:36 -0400

    Set up show page with router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
 import "./App.css";
 import Home from "./pages/Home/Home";
-import { BrowserRouter } from "react-router-dom";
+import Show from "./pages/Show/Show";
 
 class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
-				<Home />
+				<div>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/show" component={Show} />
+				</div>
 			</BrowserRouter>
 		);
 	}
---
+++ src/pages/Show/Show.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./Show.css";
+
+import Header from "../Home/Header/Header";
+
+class Show extends Component {
+	render() {
+		return (
+			<div className="home">
+				<Header />
+				<div className="home_body col-xs-12 middle-xs">
+					<h1 class="title">Show Page</h1>
+					<p class="home_text">
+					
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Show;
