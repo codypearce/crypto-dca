09b06535d1f951d7c6215392997b34eeefb53ea0
codypearce <outis161@gmail.com>
2018-10-23 08:57:28 -0400

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return <Home />;
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<Home />
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
