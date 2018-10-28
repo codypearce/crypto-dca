599843c6c07303d8e4333104a20cec1284ef18a8
codypearce <outis161@gmail.com>
2018-10-28 09:18:42 -0400

    Fix coindata values, create array  and handle data on show page

--- src/pages/Show/Show.js
+++ src/pages/Show/Show.js
@@ -6,21 +6,54 @@ import queryString from "query-string";
 import APIURL from "../../constants/API";
 
 class Show extends Component {
+	state = {
+		params: null
+	};
 	componentDidMount() {
-		const parsed = queryString.parse(this.props.location.search);
-		// const coinData = await this.getCoinData();
-	}
-	async getCoinData() {
-		const { startDate, endDate } = this.state;
+		const params = queryString.parse(this.props.location.search);
 
-		const dateString = `?start=${startDate.format(
-			"YYYY-MM-DD"
-		)}&end=${endDate.format("YYYY-MM-DD")}`;
+		this.setState({
+			...params
+		});
+		this.getCoinData(params.start, params.end);
+	}
+	async getCoinData(startDate, endDate) {
+		const dateString = `?start=${startDate}&end=${endDate}`;
 		const url = `${APIURL}${dateString}`;
 
 		const coinRepsonse = await fetch(url);
 		const coinJson = await coinRepsonse.json();
-		return coinJson;
+		let coinDataArray = [];
+		for (const key of Object.keys(coinJson.bpi)) {
+			coinDataArray.push({
+				date: key,
+				value: coinJson.bpi[key]
+			});
+		}
+		this.setState({ coinData: coinDataArray }, () => {
+			this.initializeData();
+		});
+	}
+	initializeData() {
+		const { amount, freq, coinData } = this.state;
+		// console.log(this.state);
+		// console.log(coinData.length, freq);
+		let dollarAmountInvested = 0;
+		let coinAmount = 0;
+
+		let dataArr = [];
+		for (let i = 0; i < coinData.length; i += Number(freq)) {
+			dollarAmountInvested += Number(amount);
+			coinAmount += amount / coinData[i].value;
+			dataArr.push({
+				dollarAmountInvested,
+				coinAmount,
+				coinValue: coinData[i].value,
+				investedValue: coinAmount * coinData[i].value,
+				date: coinData[i].date
+			});
+		}
+		console.log(dollarAmountInvested, coinAmount, dataArr);
 	}
 	render() {
 		return (
